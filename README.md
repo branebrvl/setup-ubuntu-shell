0b657c174d7a8c5e0594aa7a645b5b92934c2817
branislav.vladisavljev <branebrvl@gmail.com>
2013-09-05 00:43:15 -0700

    added setup.sh

---
+++ README.md
@@ -0,0 +1,12 @@
+setup
+=========
+Clone and run this on a new instance running Ubuntu to
+configure both the machine and your individual development environment as
+follows:
+
+```sh
+cd $HOME
+sudo apt-get install -y git-core
+git clone git@github.com:branebrvl/setup-ubuntu-shell.git
+./setup-ubuntu-shell/setup.sh   
+```
---
+++ setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Simple setup.sh for configuring Ubuntu instance
+# for headless setup. 
+
+# Install nvm: node-version manager
+# https://github.com/creationix/nvm
+sudo apt-get install -y git
+sudo apt-get install -y curl
+curl https://raw.github.com/creationix/nvm/master/install.sh | sh
+
+# Load nvm and install latest production node
+source $HOME/.nvm/nvm.sh
+nvm install v0.10.12
+nvm use v0.10.12
+
+# Install jshint to allow checking of JS code within emacs
+# http://jshint.com/
+npm install -g jshint
+
+# Install rlwrap to provide libreadline features with node
+# See: http://nodejs.org/api/repl.html#repl_repl
+sudo apt-get install -y rlwrap
+
+# Install Heroku toolbelt
+# https://toolbelt.heroku.com/debian
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
+# git pull and install dotfiles as well
+cd $HOME
+if [ -d ./dotfiles/ ]; then
+    mv dotfiles dotfiles.old
+fi
+
+git clone git@github.com:branebrvl/dotfiles.git
+./dotfiles/setup.sh
